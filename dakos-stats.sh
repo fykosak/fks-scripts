fc4efa4a641b00a7250276e0ccd3581c9dde9353
Michal Koutný <xm.koutny@gmail.com>
2013-04-21 22:15:47 +0200

    support for missing stats, reduced manual work

--- dakos-stats.sh
+++ dakos-stats.sh
@@ -12,7 +12,10 @@ sed -i 's/\r//' $STATSFILE
 IFS=";"
 cat $STATSFILE | while read batch problem max avg people ; do
 	[ "x$problem" = "x" ] && continue
-	[ "$avg" = "NaN" ] && continue
+	# note slashes are escaped as they're used in sed expression
+	[ "$avg" = "NaN" ] && avg="N\/A"
+	[ "$people" = "NaN" -o "$people" = "0" ] && people="N\/A" # we don't distinguish no competititors and missing stats :-(
+
 	FILE="${PROBLEMS}/problem$batch-$problem.tex"
 	[ -f "$FILE" ] || continue
 	
