c7d502c42dc79c7253dceb1cb5791ef9f2c89072
Tomáš Pikálek <tpikalek@gmail.com>
2013-06-03 20:49:24 +0200

    vlnka.sh - nová slova, nové funkce

--- vlnka.sh
+++ vlnka.sh
@@ -2,186 +2,281 @@
 
 # seznam zkratek, za kterými má být vlnka
 ZKRATCZ="
-	tj. tzn. tzv. pozn. např. popř. př. kupř. č. obr.
+	tj. tzn. tzv. pozn. pozn.: např. např.: popř. př. kupř. č. obr. resp.
 "
 ZKRATSK="
-	tj. tzn. tzv. pozn. napr. popr. pr. kupr. č. obr.
+	tj. tzn. tzv. pozn. pozn.: napr. napr.: popr. pr. kupr. č. obr. resp.
 "
-
 
 # seznam slov před a za matematikou
 PZMATCZ="
 	bod bodu bode bodě bodem body bodů bodům body bodech
-	úhel úhlu úhel úhle úhlem úhlů úhlům úhly úhlech
-	sklon sklonu sklone sklonem sklony sklonů sklonům sklonech
-	vzdálenost vzdálenosti vzdáleností vzdálenostem vzdálenostech vzdálenostmi
-	síla síly síle sílu sílo silou sílou síly sil silám sílám silách sílách silami sílami
-	moment momentu momente momentě momentem momenty momentů momentům momentech
-	velikost velikosti velikostí velikostem velikostech velikostmi
-	vztah vztahu vztahem vztahy vztahů vztahům vztazích
-	vzorec vzorce vzorci vzorcem vzorců vzorcům vzorcích
-	výraz výrazu výraze výrazem výrazy výrazů výrazům výrazech
-	rovnice rovnici rovnicí rovnic rovnicích rovnicemi
-	rovnost rovnosti rovností rovnostem rovnostech rovnostmi
-	energie energii energií energiím energiích energiemi
-	hmotnost hmotnosti hmotností hmotnostem hmotnostech hmotnostmi
-	rychlost rychlosti rychlostí rychlostem rychlostech rychlostmi
-	zrychlení zrychlením zrychleních zrychleními
-	hybnost hybnosti hybností hybnostem hybnostech hybnostmi
-	součet součtu součte součtem součty součtů součtům součtech
-	rozdíl rozdílu rozdíle rozdílem rozdíly rozdílů rozdílům rozdílech
-	podíl posílu počíle podílem podíly podílů podílům podílech
-	součin součinu součine součinem součiny součinů součinům součinech
-	konstanta konstanty konstantě konstantu konstanto konstantou konstant konstanám konstantách konstantami
-	exponent exponentu exponente exponentem exponenty exponentům exponentech
+	čas času čase časem časy časů časům časech
 	číslo čísla číslu čísle číslem čísel číslům číslech čísly
-	veličina veličiny veličině veličinu veličino veličinou veličin veličinám veličinách veličinami
-	proměnná proměnné poměnnou proměnných proměnným proměnnými
-	neznámá neznámé neznámou neznánými neznámým
+	definovat definujeme definujme definovali definice definicí
+	délka délky délce délku délko délkou délek délkám délkách délkami dlouhý dlouhá dlouhé
+	doba doby době dobu dobo době dobou dob dobám dobách dobami
+	energie energii energií energiím energiích energiemi
+	existuje
+	exponent exponentu exponente exponentem exponenty exponentům exponentech
+	frekvence  frekvenci frekvencí frekvencím frekvencích frekvencemi
+	hloubka hloubky hloubce hloubku hloubko hloubce hloubkou hloubek hloubkám hloubkách hloubkami hluboký hluboká hluboké
+	hmotnost hmotnosti hmotností hmotnostem hmotnostech hmotnostmi
+	hodnota hodnoty hodnotě hodnotu hodnoto hodnotou hodnot hodnotám hodnotách hodnotami
+	hrana hrany hraně hranu hrano hranou hran hranám hranách hranami
+	hustota hustoty hustotě hustotu hustoto hustotou hustot hustotám hustotách hustotami
+	hybnost hybnosti hybností hybnostem hybnostech hybnostmi
+	index indexu indexe indexem indexy indexům indexech
+	interval intervalu intervale intervalem intervaly intervalů intervalům intervalech
+	je bude
+	jej ho
+	jsou
+	kladný kladná kladné kladnému kladným kladných
+	konstanta konstanty konstantě konstantu konstanto konstantou konstant konstantám konstantách konstantami
+	moment momentu momente momentě momentem momenty momentů momentům momentech
+	náboj náboje náboji nábojem nábojům nábojích náboji
+	napětí napětím napětích napětími
+	neplatí
+	neznámá neznámé neznámou neznámými neznámým
+	objem objemu objeme objemem objemy objemů objemům objemech
+	parametr parametru parametre parametrem parametry parametrům parametrech
+	platí platit
+	podíl podílu podíle podílem podíly podílů podílům podílech
+	pole polí poli polem polím polích
+	poloha polohy poloze polohu poloho polohou poloh polohám polohách polohami
 	poloměr poloměru poloměre poloměrem poloměry poloměrů poloměrům poloměrech
+	proměnná proměnné proměnnou proměnných proměnným proměnnými
+	proud proudu proude proudem proudy proudů proudům proudech
 	průměr průměru průměre průměrem průměry průměrů průměrům průměrech
 	průmět průmětu průměte průmětem průměty průmětům průmětech
-	délka délky délce délku délko délkou délek délkám délkách délkami dlouhý dlouhá dlouhé
-	šířka šířky šířce šírku šířko šířkou šířek šířkám šířkách šířkami široký široká široké
-	výška výšky výšce výšku výško výškou výšky výšek výškám výškách výškami vysoký vysoká vysoké
-	hloubka hloubky hloubce hloubku hloubko hloubce hloubkou hloubek hloubkám hloubkách hloubkami hluboký hluboká hluboké
-	hustota hustoty hustotě hustotu hustoto hustotou hustot hustotám hustotách hustotami
-	napětí napětím napětích napětími
-	proud proudu proude proudem proudy proudů proudům proudech
-	náboj náboje náboji nábojem nábojům nábojích náboji
-	čas času čase časem časy časů časům časech
-	doba doby době dobu dobo době dobou dob dobám dobách dobami
+	přepočet přepočtu přepočte přepočtem přepočty přepočtů přepočtům přepočtech
+	rovnice rovnici rovnicí rovnic rovnicích rovnicemi
+	rovnost rovnosti rovností rovnostem rovnostech rovnostmi
+	rozdíl rozdílu rozdíle rozdílem rozdíly rozdílů rozdílům rozdílech
+	rozměr rozměru rozměre rozměrem rozměry rozměrů rozměrům rozměrech
+	rychlost rychlosti rychlostí rychlostem rychlostech rychlostmi
+	síla síly síle sílu sílo silou sílou síly sil silám sílám silách sílách silami sílami
+	sklon sklonu sklone sklonem sklony sklonů sklonům sklonech
+	složka složky složce složku složko složkou složek složkách složkami
+	směr směru směre směrem směry směrů směrům směrech
+	součet součtu součte součtem součty součtů součtům součtech
+	součin součinu součine součinem součiny součinů součinům součinech vynásobení vynásobené vynásobená vynásobena
+	souřadnice souřadnici souřadnicí souřadnic souřadnicím souřadnicích souřadnicemi
+	strana strany straně stranu strano stranou stran stranám stranách stranami
+	střed středu střede středem středy středů středům středech
+	substituce substituci substitucí substitucím substitucích substitucemi
+	šířka šířky šířce šířku šířko šířkou šířek šířkám šířkách šířkami široký široká široké
 	teplota teploty teplotě teplotu teploto teplotou teplot teplotám teplotách teplotami
-	objem objemu objeme objemem objemy objemů objemům objemech
-	je
-	jsou
+	tlak tlaku tlalem tlaky tlaků tlakům tlacích
+	tloušťka tloušťky tloušťce tloušťku tloušťko tloušťkou tlouštěk tloušťkám tloušťkách tloušťkami
+	trvat trvá trvalo trvala
+	úhel úhlu úhel úhle úhlem úhlů úhlům úhly úhlech
+	veličina veličiny veličině veličinu veličino veličinou veličin veličinám veličinách veličinami
+	velikost velikosti velikostí velikostem velikostech velikostmi
+	vychází
+	vyjde
+	výraz výrazu výraze výrazem výrazy výrazů výrazům výrazech
+	výška výšky výšce výšku výško výškou výšky výšek výškám výškách výškami vysoký vysoká vysoké
+	vzdálenost vzdálenosti vzdáleností vzdálenostem vzdálenostech vzdálenostmi
+	vzorec vzorce vzorci vzorcem vzorců vzorcům vzorcích
+	vztah vztahu vztahem vztahy vztahů vztahům vztazích
+	záporný záporná záporné zápornému záporným záporných
+	zrychlení zrychlením zrychleních zrychleními
 "
 
 PZMATSK="
 	bod bodu bode bode bodom body bodov bodom body bodoch
-	uhol uhla uhol uhla uhlom uhlov uhlom uhly uhloch
-	sklon sklonu sklon sklonom sklony sklonov sklonom sklonoch
-	vzdialenosť vzdialenosti vzdialenosťou vzdialenostiam vzdialenostiach vzdialenosťami
-	sila sily sile silu sila silou silou sily síl silám silám silách hrúbkach silami silami
-	moment momentu momente momente momentom momenty momentov momentom momentoch
-	veľkosť veľkosti veľkostí veľkostiam veľkostiach veľkosťami
-	vzťah vzťahu vzťahom vzťahy vzťahov vzťahom vzťahoch
-	vzorec vzorca vzorci vzorcom vzorcov vzorcom vzorkách
-	výraz výrazu výrazoch výrazom výrazy výrazov výrazom výrazoch
-	rovnice rovnicu rovnicou rovníc rovniciach rovnicami
-	rovnosť rovnosti rovnosťou rovnosť rovnosť rovnosť
-	energie energiu energiou energiám energiách energiami
-	hmotnosť hmotnosti hmotnosťou hmotnosť hmotnosť hmotnosťami
-	rýchlosť rýchlosti rýchlosťou rýchlostiam rýchlostiach rýchlosťami
-	zrýchlenie zrýchlením zrýchleniach zrýchleniami
-	hybnosť hybnosti hybnosťou hybnosť hybnosť hybnosť
-	súčet súčtu Souci súčtom súčty súčtov súčtov súčtoch
-	rozdiel rozdielu Rozdiel Rozdiel rozdiely rozdielov rozdielom rozdieloch
-	podiel posilami počíle podielom podiely podielov podielom podieloch
-	súčin súčinu súčin súčinom súčiny súčinov součinům kombináciách
-	konštanta konštanty konštante konštantu konštanty konštantou konštánt konstanám konštantách konštantami
-	exponent exponentu exponente exponentom exponentov exponentom exponentoch
+	čas času čase časom časy časov časom časoch
 	číslo čísla číslu čísle číslom čísel číslam číslach číslami
-	veličina veličiny veličine veličinu veličiny veličinou veličín veličinám veličinách veličinami
-	premenná premenné poměnnou premenných premenným premennými
-	neznáma neznáme neznámu neznánými neznámym
+	definovať definujeme definujme definovali definície definíciou
+	dĺžka dĺžky dĺžke dľžka dľžka dľžka dĺžok dĺžkam dĺžkach dĺžkami dlhý dlhá dlhé
+	čas doby dobe dobu dobo dobe dobou čias dobám dobách dobami
+	energie energiu energiou energiám energiách energiami
+	existuje
+	exponent exponentu exponente exponentom exponentov exponentom exponentoch
+	frekvencia frekvenciu frekvenciou frekvenciám frekvenciách frekvenciami
+	hĺbka hĺbky hĺbke hĺbku hĺbko hĺbke hĺbkou hĺbok hĺbkam hĺbkach hĺbkami hlboký hlboká hlboké
+	hmotnosť hmotnosti hmotnosťou hmotnosť hmotnosť hmotnosťami
+	hodnota hodnoty hodnote hodnota Hodnota hodnotou hodnôt hodnotám hodnotách hodnotami
+	hrana hrany hrane hranu hrano hranou hrán hranám hranách hranami
+	hustota hustoty hustote hustotu hustoty hustotou hustôt hustoty hustotách hustotami
+	hybnosť hybnosti hybnosťou hybnosť hybnosť hybnosť
+	index Index Index indexom indexy indexom indexoch
+	interval intervalu intervale intervalom intervaly intervalov intervalom intervaloch
+	ich bude
+	ho ho
+	sú
+	kladný kladná kladné kladnému kladným kladných
+	konštanta konštanty konštante konštantu konštanty konštantou konštánt konštantám konštantách konštantami
+	moment momentu momente momente momentom momenty momentov momentom momentoch
+	náboj náboja náboji nábojom nábojom nábojoch nábojmi
+	napätie napätím napätiach napätiami
+	neplatí
+	neznáma neznáme neznámu neznámymi neznámym
+	objem objemu objem objemom objemy objemov objemom objemoch
+	parameter parametra parametre parametrom parametrami parametrom parametroch
+	platí platiť
+	podiel podielu Podiel Podiel podiely podielov podielom podieloch
+	pole polí poli poľom poliam poliach
+	poloha polohy polohe polohu Position Umiestnenie polôh polohám polohách polohami
 	polomer polomeru polomer polomerom polomery polomerov polomerom polomeroch
+	premenná premenné premennou premenných premenným premennými
+	prúd prúdu Prúd prúdom prúdy prúdov prúdom prúdoch
 	priemer priemeru Rozmer Rozmer priemery priemerov priemerov priemeroch
 	priemet priemetu priem priemetom priemety priemetom Priemet
-	dĺžka dĺžky dĺžke dľžka dľžka dľžka dĺžok dĺžkam dĺžkach dĺžkami dlhý dlhá dlhé
-	šírka šírky šírke šírka šírka šírkou šírok šírkam šírkach šírkami široký široká široké
-	výška výšky výške výška výška výška výška výšok výškam výškach výškami vysoký vysoká vysoké
-	hĺbka hĺbky hĺbke hĺbku hĺbko hĺbke hĺbkou hĺbok hĺbkam hĺbkach hĺbkami hlboký hlboká hlboké
-	hustota hustoty hustote hustotu hustoty hustotou hustôt hustoty hustotách hustotami
-	napätie napätím napätiach napätiami
-	prúd prúdu Prúd prúdom prúdy prúdov prúdom prúdoch
-	náboj náboja náboji nábojom nábojom nábojoch nábojmi
-	čas času čase časom časy časov časom časoch
-	čas doby dobe dobu dobo dobe dobou čias dobám dobách dobami
+	prepočet prepočte prepočíta prepočtom prepočty prepočtov prepočtom prepočtoch
+	rovnice rovnicu rovnicou rovníc rovniciach rovnicami
+	rovnosť rovnosti rovnosťou rovnosť rovnosť rovnosť
+	rozdiel rozdielu Rozdiel Rozdiel rozdiely rozdielov rozdielom rozdieloch
+	rozmer rozmeru rozmery Rozmery rozmery rozmerov rozmerom rozmeroch
+	rýchlosť rýchlosti rýchlosťou rýchlostiam rýchlostiach rýchlosťami
+	sila sily sile silu sila silou silou sily síl silám silám silách hrúbkach silami silami
+	sklon sklonu sklon sklonom sklony sklonov sklonom sklonoch
+	zložka zložky zložke zložku zložku zložkou zložiek zložkách zložkami
+	smer smeru smere smerom smermi smerov líniám smeroch
+	súčet súčtu Souci súčtom súčty súčtov súčtov súčtoch
+	súčin súčinu súčin súčinom súčiny súčinov součinům kombináciách vynásobení vynásobenej vynásobená vynásobená
+	súradnice súradnicu súradnicou súradníc súradniciam súradniciach súradnicami
+	strana strany strane strana Strana stranou strán stranám stranách stranami
+	stred stredu strede stredom stredy stredov stredom stredoch
+	substitúcia substitúciu substitúciou substitucím substitúciou substitúciami
+	šírka šírky šírke šírku šírka šírka šírok šírkam šírkach šírkami široký široká široké
 	teplota teploty teplote teplotu Teplota Teplota teplôt teplotám teplotách teplotami
-	objem objemu objem objemom objemy objemov objemom objemoch
-	je
-	sú
+	tlak tlaku tlalem tlaky tlakov tlakom tlakoch
+	hrúbka hrúbky hrúbke hrúbku hrúbku hrúbkou hrúbok hrúbkam hrúbkach hrúbkami
+	trvať trvá trvalo trvala
+	uhol uhla uhol uhla uhlom uhlov uhlom uhly uhloch
+	veličina veličiny veličine veličinu veličiny veličinou veličín veličinám veličinách veličinami
+	veľkosť veľkosti veľkostí veľkostiam veľkostiach veľkosťami
+	vychádza
+	vyjde
+	výraz výrazu výrazoch výrazom výrazy výrazov výrazom výrazoch
+	výška výšky výške výška výška výška výška výšok výškam výškach výškami vysoký vysoká vysoké
+	vzdialenosť vzdialenosti vzdialenosťou vzdialenostiam vzdialenostiach vzdialenosťami
+	vzorec vzorca vzorci vzorcom vzorcov vzorcom vzorkách
+	vzťah vzťahu vzťahom vzťahy vzťahov vzťahom vzťahoch
+	záporný záporná záporné zápornému záporným záporných
+	zrýchlenie zrýchlením zrýchleniach zrýchleniami
 "
 
 # seznam slov před matematikou
 PRMATCZ="
-	podělíme podělili
-	vydělíme vydělili
-	přičteme přičetli
-	položíme položili
-	označíme označili
-	porovnáme porovnali
-	číselně
-	dosazení
-	dosadíme dosadili
-	rovno rovna rovná rovny rovnají
-	tedy
-	na
-	pro
 	asi
-	zrhuba
-	přibližně
-	téměř
-	skoro
+	být
+	cca
+	číselně
+	do
+	dosadíme dosadili
+	dosazení
+	dosazení dosazením
+	jako
+	na
 	než
+	od
+	označíme označili značí značíme označme
+	pak
+	podělíme podělili
+	položíme položili
+	pomocí
+	porovnáme porovnali
+	pro
 	průměrně
+	předpoklad
+	přesně přesněji nejpřesněji přesnější nejpřesnější
+	přibližně
+	přičteme přičetli
+	psát
+	rok roku roce rokem roky roků rokům rocích
+	rovno rovna rovná rovny rovnají
+	různý
+	skoro
+	tedy
+	téměř
+	uvažovat uvažujeme uvažujme
+	volíme zvolíme zvolili zvolme
+	vydělíme vydělili
+	výsledek výsledku výsledkem výsledky výsledků výsledkům výsledcích
+	za
+	zároveň
+	zhruba
+	že
 "
 PRMATS="
-	podelíme podelili
-	vydelíme vydelili
-	prirátame pripočítali
-	položíme položili
-	označíme označili
-	porovnáme porovnali
-	číselne
-	dosadení
-	dosadíme dosadili
-	presne rovná rovná rovny rovnajú
-	teda
-	na
-	pre
 	asi
-	zrhuba
-	približne
-	takmer
-	skoro
+	byť
+	cca
+	číselne
+	do
+	dosadíme dosadili
+	dosadení
+	dosadení dosadením
+	ako
+	na
 	než
+	od
+	označíme označili značí značíme označme
+	potom
+	podelíme podelili
+	položíme položili
+	pomocou
+	porovnáme porovnali
+	pre
 	priemerne
+	predpoklad
+	presne presnejšie najpresnejšie presnejšie najpresnejší
+	približne
+	prirátame pripočítali
+	písať
+	rok roka roku rokom roky rokov rokom rokoch
+	presne rovná rovná rovny rovnajú
+	rôzny
+	skoro
+	teda
+	takmer
+	uvažovať uvažujeme uvažujme
+	volíme zvolíme zvolili zvoľme
+	vydelíme vydelili
+	výsledok výsledku výsledkom výsledky výsledkov výsledkom výsledkoch
+	za
+	zároveň
+	zhruba
+	že
 "
 
 # seznam slov za matematikou
 ZAMATCZ="
 	je jsou
+	jeho její
 "
 ZAMATSK="
 	je sú
+	jeho jej
 "
 
 # seznam názvů odkazů, za kterými má být vlnka
 PRODKCZ="
-	obrázek obrázku obrázky obrázkům
-	tabulka tabulce tabulky tabulkách tabulkám
-	výraz výrazu výraze výrazem výrazy výrazů výrazům výrazech
-	vztah vztahu vztahem vztahy vztahů vztahům vztazích
-	vzorec vzorce vzorci vzorcem vzorců vzorcům vzorcích
-	výraz výrazu výraze výrazem výrazy výrazů výrazům výrazech
-	rovnice rovnici rovnicí rovnic rovnicích rovnicemi
-	podle
 	dle
+	podle
+	rovnice rovnici rovnicí rovnic rovnicích rovnicemi
+	tabulka tabulce tabulky tabulkách tabulkám
 	viz
+	výraz výrazu výraze výrazem výrazy výrazů výrazům výrazech
+	výraz výrazu výraze výrazem výrazy výrazů výrazům výrazech
+	vzorec vzorce vzorci vzorcem vzorců vzorcům vzorcích
+	vztah vztahu vztahem vztahy vztahů vztahům vztazích
+	obrázek obrázku obrázky obrázkům
 "
 PRODKSK="
-	obrázok obrázku obrázky obrázkom
-	tabuľka tabuľke tabuľky tabuľkách tabuľkám
-	výraz výrazu výrazoch výrazom výrazy výrazov výrazom výrazoch
-	vzťah vzťahu vzťahom vzťahy vzťahov vzťahom vzťahoch
-	vzorec vzorca vzorci vzorcom vzorcov vzorcom vzorkách
-	výraz výrazu výrazoch výrazom výrazy výrazov výrazom výrazoch
+	podľa
+	podľa
 	rovnice rovnicu rovnicou rovníc rovniciach rovnicami
-	podľa
-	podľa
+	tabuľka tabuľke tabuľky tabuľkách tabuľkám
 	pozri
+	výraz výrazu výrazoch výrazom výrazy výrazov výrazom výrazoch
+	výraz výrazu výrazoch výrazom výrazy výrazov výrazom výrazoch
+	vzorec vzorca vzorci vzorcom vzorcov vzorcom vzorkách
+	vzťah vzťahu vzťahom vzťahy vzťahov vzťahom vzťahoch
+	obrázok obrázku obrázky obrázkom
 "
 
 
@@ -189,12 +284,12 @@ PRODKSK="
 ZKRAT="$ZKRATCZ $ZKRATSK"
 ZKRAT="$(echo $ZKRAT | sed -r -e 's/ +/|/g')"
 ZKRAT="$(echo $ZKRAT | sed -r -e 's/\./[\.]/g')"
-PRMAT="$PZMATCZ $PZMATSK $PRMATCZ $PRMATSK"
-PRMAT=$(echo $PRMAT | sed -r -e 's/ +/|/g')
+PRMAT="$PZMATCZ $PZMATSK $PRMATCZ $PRMATSK $ZKRATCZ $ZKRATSK"
+PRMAT=$(echo $PRMAT | sed -r -e 's/[ \t]+/|/g')
 ZAMAT="$PZMATCZ $PZMATSK $ZAMATCZ $ZAMATSK"
-ZAMAT=$(echo $ZAMAT | sed -r -e 's/ +/|/g')
+ZAMAT=$(echo $ZAMAT | sed -r -e 's/[ \t]+/|/g')
 PRODK="$PRODKCZ $PRODKSK"
-PRODK=$(echo $PRODK | sed -r -e 's/ +/|/g')
+PRODK=$(echo $PRODK | sed -r -e 's/[ \t]+/|/g')
 
 for file in "$@"
 do
@@ -202,8 +297,16 @@ do
 	sed -r -i -e "N;s/([[:alpha:]])[ \n\t]+--[ \n\t]+([[:alpha:]])/\1~--~\2/gI;P;D;" "$file" # kolem pomlčky
 	sed -r -i -e "N;s/([ \n\t~]+)($ZKRAT)([ \n\t]+)/\3\1\2~/gI;P;D;" "$file" # zkratky
 	sed -r -i -e "N;s/([ \n\t~]+)($PRMAT)([ \n\t]+)[\$]/\3\1\2~\$/gI;P;D;" "$file" # před matematikou
+	sed -r -i -e "N;s/([ \n\t~]+)($PRMAT)([ \n\t~]+)([[:alpha:]]+)([ \n\t]+)[\$]/\1\3\5\2\3\4~\$/gI;P;D;" "$file" # před matematikou + 1 další slovo
 	sed -r -i -e "N;s/[\$]([ \n\t]+)($ZAMAT)([ \n\t~]+)/\$~\2\3\1/gI;P;D;" "$file" # za matematikou
 	sed -r -i -e "N;s/([ \n\t~]+)($PRODK)[ \n\t~]+([\\]ref|[\\]eqref|\([\\]ref)/\1\2~\3/gI;P;D;" "$file" # před odkazy
+	sed -r -i -e "N;s/\([\\]ref\{([^\}]*)\}\)/\\\eqref\{\1\}/gI;P;D;" "$file" # nahradit ošklivé odkazy na rovnice
 	sed -r -i -e "N;s/~{2,}/~/gI;P;D;" "$file" # opravit násobné vlnky
-	sed -r -i -e "N;s/[ \n\t]*~[ \n\t]*/~/gI;P;D;" "$file" # opravit mezery kolem vlnek
+	sed -r -i -e "N;s/[ \n\t]*~[ \n\t]*/~/gI;P;D;" "$file" # smazat mezery kolem vlnek
+	sed -r -i -e "N;s/\n            /\n\t\t\t/gI;P;D;" "$file" # mezery na 3 odsazení
+	sed -r -i -e "N;s/\n        /\n\t\t/gI;P;D;" "$file" # mezery na 2 odsazení
+	sed -r -i -e "N;s/\n    /\n\t/gI;P;D;" "$file" # mezery na 1 odsazení
+	sed -r -i -e "N;s/ {2,}/ /gI;P;D;" "$file" # opravit násobné mezery
+	sed -r -i -e "N;s/ *\n/\n/gI;P;D;" "$file" # smazat mezery na konci řádku
+	sed -r -i -e "N;s/\n */\n/gI;P;D;" "$file" # smazat mezery na začátku řádku
 done
