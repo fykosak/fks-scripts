54a19766c1f056ff4d6d03737f703d2857488cc3
Michal Koutný <xm.koutny@gmail.com>
2014-05-20 08:04:54 +0200

    Fix in pkg upload

--- fks-pkg-upload.sh
+++ fks-pkg-upload.sh
@@ -5,7 +5,7 @@ source /etc/fks/config
 DIST=fks
 SECTION=main # ?? it's required
 
-if [ "x$1" = "x" -o "$1$ = "-h" ] ; then
+if [ "x$1" = "x" -o "$1" = "-h" ] ; then
 	echo "Usage: $0 [-d dist] deb-file(s) ..."
 	echo "   Default dist is '$DIST'."
 	exit 1
@@ -24,5 +24,11 @@ scp $@ $pkg_user@$pkg_server:/tmp/
 for file in "$@" ; do
 	ssh $pkg_user@$pkg_server reprepro -S $SECTION -b $pkg_path/$DIST includedeb $DIST /tmp/$file
 done
+
+# sign the repo
+scp $pkg_user@$pkg_server:$pkg_path/$DIST/dists/$DIST/Release /tmp
+gpg -o /tmp/Release.gpg -ba /tmp/Release 
+scp /tmp/Release.gpg $pkg_user@$pkg_server:$pkg_path/$DIST/dists/$DIST/Release.gpg
+rm -f /tmp/Release /tmp/Release.gpg
 
 ssh $pkg_user@$pkg_server rm "/tmp/*.deb"
