ff8a188ce8d540b5453139e0227f087d37ef397f
Michal Koutný <xm.koutny@gmail.com>
2014-09-20 15:48:23 +0200

    Exkluzivní template

--- fks-initBL.sh
+++ fks-initBL.sh
@@ -112,8 +112,7 @@ if [ "$1" = "b" ]; then
 	cd "../$PROBLEMS_DIR"
 	for P in $(seq 1 $PROB_COUNT); do
 		FILENAME="problem$2-$P.tex"
-		TEMPLATE="$TEMPLATE_PATH/year/problems/problemB-P.tex"
-		[ -f $TEMPLATE ] || TEMPLATE=${TEMPLATE}.$seminar # fallback to universal version
+		TEMPLATE="$TEMPLATE_PATH/year/problems/problemB-P.tex.$seminar"
 	   	cp "$TEMPLATE" "$FILENAME"
 		sed -i "s/\\\\probbatch{B}/\\\\probbatch{$2}/;s/\\\\probno{P}/\\\\probno{$P}/" $FILENAME
 	done
