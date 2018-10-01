11bdd86b51c0de1174889e1538d51c987a50f9a4
Tuaki <tuaki@fykos.cz>
2018-10-01 21:45:20 +0200

    úprava Fyziklani na FOL

--- scripts/bin/fks-initFOLTempl.sh
+++ scripts/bin/fks-initFOLTempl.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source /etc/fks/config
 
 if [ $# -le 3 ]; then
-    echo "Usage: fks-initTempl.sh <year> <batch> <probType:J|N|P|E|S> <name in czech incl. diacritics\"\">"
+    echo "Usage: fks-initFOLTempl.sh <year> <name in czech incl. diacritics\"\">"
     exit -1
 fi
 
@@ -14,9 +14,7 @@ if [ $? -ne "0" ]; then
 fi
 
 year=$1
-batch=$2
-ptype=$3
-name=$4
+name=$2
 fname=`iconv -f utf8 -t ascii//TRANSLIT <<< $name | 
         sed "s/ /_/g" | 
         tr '[:upper:]' '[:lower:]' |
@@ -28,40 +26,19 @@ if [[ !($year =~ ^[0-9]+$) ]]; then
     exit -1
 fi
 
-if [[ !($batch =~ ^[0-9]+$) ]]; then
-    echo "Invalid batch: "$batch
-    exit -1
-fi
-
-if [[ !($ptype =~ ^[J|N|P|E|S]$) ]]; then
-    echo "Invalid type: "$ptype
-    exit -1
-fi
-
-# set problem number
-if [ $ptype == "J" ]; then pno=1;fi
-if [ $ptype == "N" ]; then pno=3;fi
-if [ $ptype == "P" ]; then pno=6;fi
-if [ $ptype == "E" ]; then pno=7;fi
-if [ $ptype == "S" ]; then pno=8;fi
-
 # echo input params
 echo "Creating problem:"
 echo "  Year:  "$year 
-echo "  Batch: "$batch
-echo "  Type:  "$ptype
 echo "  Name:  "$name
 echo "  Fname: "$fname
 echo -n "Do you want to create template [Y/n]? "
 read answer
 if [ $answer == "Y" ];then
     echo "Creating problem file."
-    id=`ls "in/series/"$ptype"-R"$year"S"$batch"-"*"-"*".tex" 2>/dev/null | wc -l`
-    fname="in/series/"$ptype"-R"$year"S"$batch"-"$id"-"$fname".tex"
-    cp $TEMPLATE_PATH/problem-templ.tex $fname
+    id=`ls "in/series/R"$year"-"*"-"*".tex" 2>/dev/null | wc -l`
+    fname="in/series/R"$year"-"$id"-"$fname".tex"
+    cp $TEMPLATE_PATH/problem-FOLtempl.tex $fname
     sed -i "s/YY/"$year"/g" $fname
-    sed -i "s/BB/"$batch"/g" $fname
-    sed -i "s/PP/"$pno"/g"   $fname
     sed -i "s/probname{/probname{$name/g" $fname
     echo "Problem file created."
     exit 0
