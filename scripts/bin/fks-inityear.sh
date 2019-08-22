54bcb2943bfd44b568e42ef5314263f220650c0b
Headary <41702931+Headary@users.noreply.github.com>
2019-08-22 20:55:42 +0200

    Vyfuk integration (#15)
    * fix batchno + vyfuk problems count
    
    * vyfuk/fykos integration

--- scripts/bin/fks-inityear.sh
+++ scripts/bin/fks-inityear.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+source /usr/share/fks/semyr
 source /etc/fks/config
 
 PROBLEMS_DIR=problems
+
+#setup year & seminar
+semyr ""
 
 # Initializes a year structure in current directory
 
@@ -14,7 +18,6 @@ fi
 
 # create empty year structure
 mkdir -p problems
-mkdir -p results
 mkdir -p data
 mkdir -p todo
 mkdir -p pdf
@@ -22,12 +25,26 @@ cp -rT "$TEMPLATE_PATH/year/problems/graphics" "./problems/graphics"
 cp "$TEMPLATE_PATH/year/Deadline_xml.inc" .
 cp "$TEMPLATE_PATH/year/Makefile.inc" .
 cp "$TEMPLATE_PATH/year/.gitignore.sample" ".gitignore"
+cp "$TEMPLATE_PATH/year/.gitattributes.sample" ".gitattributes"
 cp "$TEMPLATE_PATH/year/Makefile.conf.sample" .
 #cp "$TEMPLATE_PATH/year/Makefile.conf.sample" Makefile.conf
 cp "$TEMPLATE_PATH/year/Makefile" .
 cp "$TEMPLATE_PATH/year/data/"* ./data
 cp -r "$TEMPLATE_PATH/year/todo/"* ./todo
 cp -r "$TEMPLATE_PATH/year/pdf/"* ./pdf
+
+# keep files needed by seminar
+if [ $seminar = "fykos" ];then
+	mkdir -p results
+	find . -name "*.vyfuk" -type f -delete
+else
+	find . -name "*.fykos" -type f -delete
+fi
+
+# cut .fykos|.vyfuk
+for file in $(find . -name "*.$seminar" -type f) ; do
+	mv $file ${file%.$seminar}
+done
 
 # create dependency Makefile part
 MAKEFILE="problems/Makefile.inc"
@@ -47,9 +64,16 @@ for B in $(seq 1 $2); do
 	done
 done
 
+#SOAP files parameters
+for file in $(find . -name "*.soap" -type f) ; do
+	sed -i "s/SEM/$seminar/" 	$file
+	sed -i "s/YEAR/$rocnik/" 	$file
+done
+
+
+
 echo "Year initialized. Before commiting do not forget to modify contest specific information."
 echo "This mostly applies to:"
-echo "    - problem labels in SOAP requests(!)"
 echo "    - contest name in Makefile.conf.sample"
 
 
