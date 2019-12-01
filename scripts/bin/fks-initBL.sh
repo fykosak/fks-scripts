798e75c7bb7e31c81afc5d5a8d03e874f7199920
Adam Krška <adam@krska.com>
2019-12-01 17:47:57 +0100

    autofill studyyears and tags

--- scripts/bin/fks-initBL.sh
+++ scripts/bin/fks-initBL.sh
@@ -127,10 +127,11 @@ if [ "$1" = "b" ]; then
 				sed -i "s/\\\\probsource{SEMY}/\\\\probsource{}/" $FILENAME
 			else
 				sed -i "s/\\\\probsource{SEMY}/\\\\probsource{$SEMINAR}/" $FILENAME
+				if [ $P = "1" ]; then sed -i "s/%\\\\probstudyyears{6,7}/\\\\probstudyyears{6,7}/" $FILENAME; fi
+				if [ $P = "5" ]; then sed -i "s/\\\\probtags{}/\\\\probtags{hard}/" $FILENAME; fi
 			fi
 		done
 	fi
 fi
 
 cd ../
-
