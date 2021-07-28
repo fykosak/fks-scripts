369571437771546a81c98ca628bd2f22420051f9
Adam Krška <adam@krska.com>
2021-07-28 22:26:00 +0200

    removal of unneeded things

--- scripts/bin/fks-inityear.sh
+++ scripts/bin/fks-inityear.sh
@@ -26,10 +26,8 @@ cp "$TEMPLATE_PATH/year/Makefile.inc" .
 cp "$TEMPLATE_PATH/year/.gitignore.sample" ".gitignore"
 cp "$TEMPLATE_PATH/year/.gitattributes.sample" ".gitattributes"
 cp "$TEMPLATE_PATH/year/Makefile.conf.sample" .
-#cp "$TEMPLATE_PATH/year/Makefile.conf.sample" Makefile.conf
 cp "$TEMPLATE_PATH/year/Makefile" .
 cp "$TEMPLATE_PATH/year/data/"* ./data
-cp -r "$TEMPLATE_PATH/year/pdf/"* ./pdf
 
 # keep files needed by seminar
 if [ $seminar = "fykos" ];then
@@ -46,35 +44,13 @@ for file in $(find . -name "*.$seminar" -type f) ; do
 	mv $file ${file%.$seminar}
 done
 
-# create dependency Makefile part
-MAKEFILE="problems/Makefile-manual.inc"
-
-cat >$MAKEFILE <<END
-# Makefile part for problems dependent images
-# Problem TeX file must be first dependecy in order building process worked
-# correctly
-
-END
-
-echo >>$MAKEFILE
-for B in $(seq 1 $2); do
-	echo  >> $MAKEFILE
-	for P in $(seq 1 $1); do
-		echo "problem${B}_$P= problem$B-$P.tex" >>$MAKEFILE
-	done
-done
-
 #SOAP files parameters
-
 for file in $(find . -name "*.soap" -type f) Makefile.inc; do
-	sed -i "s/SEM/$seminar/" 	$file
-	sed -i "s/YEAR/$rocnik/" 	$file
+	sed -i "s/SEM/$seminar/" $file
+	sed -i "s/YEAR/$rocnik/" $file
 done
-
 
 
 echo "Year initialized. Before commiting do not forget to modify contest specific information."
 echo "This mostly applies to:"
 echo "    - contest name in Makefile.conf.sample"
-
-
